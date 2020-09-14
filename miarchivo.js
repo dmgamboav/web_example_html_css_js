44f7bab07e8616e11a122130c8f5ebf0acf3d07d
JUAN ARAICA NAVARRO <juan.araica.navarro@est.una.ac.cr>
2020-09-13 18:56:27 -0600

    Extraer Codigo JS a un archivo externo

--- miarchivo.js
+++ miarchivo.js
@@ -1,1 +1,20 @@
-alert("Page runnig");
+alert("Page running!");
+   
+var nombre;
+nombre=prompt("Escriba su nombre");
+document.write(nombre);
+
+function sumar(){
+
+   var num1 = 5;
+   var num2 = 8;
+  suma = num1 + num2;
+  alert("La suma es: "+suma);
+}
+
+function Saludar(nombre, edad){
+  
+  this.nombre = nombre;
+  this.edad = edad;  
+  console.log("Hola mi nombre es: "+ this.nombre + " y tengo: " + this.edad + " años." );
+}
